eefb8a61ed7a2d09325292e40035fb0a20f0b795
Nawaz-maker <khan786786nawaz@gmail.com>
2025-07-05 13:35:24 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,15 @@
+#Check if number is even or odd
+i=int(input('Enter the number: '))
+if i%2==0:
+    print(i,' is an Even number.')
+else:
+    print(i,' is an Odd number.')
+
+#sum of integers from 1 to 50
+a=sum(range(1,51))
+print(a)
+
+
+
+
+
